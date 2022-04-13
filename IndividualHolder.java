e94cc8bdc8c4fc5e2c8d780e4265d2b7de4b80e3
Papavazquez <95202332+Papavazquez@users.noreply.github.com>
2022-04-13 02:04:55 -0700

    Create IndividualHolder.java

---
+++ IndividualHolder.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+
+class individualHolder;
+{
+  private string name;
+  private int ssn;
+  
+  public individualHolder;
+  {
+  }
+  
+  public void setName(String n)
+  {
+    name = n;
+  }
+  
+  public void setSSN(int SSN)
+  {
+    ssn = SSN;
+  }
+  
+  public String n()
+  {
+    return name;
+  }
+  
+  public int SSN()
+  {
+    return ssn;
+  }
+  
+}
