2a39e357955b8869e2bfd8ca85c6d088c7bc8a6d
Papavazquez <95202332+Papavazquez@users.noreply.github.com>
2022-04-13 02:09:11 -0700

    Create CorporateHolder.java

---
+++ CorporateHolder.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+
+class corporateHolder;
+{
+  private string contact;
+  
+
+  public corporateHolder()
+  {
+  }
+  
+  public void setContact(string Contact)
+  {
+    Contact = contact;
+  }
+  
+  public string getContact()
+  {
+    return contact;
+  }
+  
+}
